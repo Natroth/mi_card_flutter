843acbcd5e9c7c3ebda5ee507762661327e59732
Natroth <nathanrth15@gmail.com>
2020-05-28 12:42:13 -0700

    first flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.blueGrey,
+        body: SafeArea(
+          child: Container(
+            color: Colors.white70,
+            child: Text('Hello'),
+            margin: EdgeInsets.all(20),
+            height: 200,
+            width: 200,
+            padding: EdgeInsets.all(75),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }

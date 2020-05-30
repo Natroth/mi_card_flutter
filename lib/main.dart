4e64e40cf313a2eb202343229d58241e07fb17c6
Natroth <nathanrth15@gmail.com>
2020-05-30 10:19:39 -0700

    completed mi card design

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueAccent,
+        backgroundColor: Colors.lightBlue,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               Container(
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
               ),
               CircleAvatar(
                 backgroundImage: AssetImage('images/IMG_9860.jpg'),
-                radius: 80,
+                radius: 85,
               ),
               Text(
                 'Nathan Roth',
@@ -36,6 +37,61 @@ class MyApp extends StatelessWidget {
                   fontFamily: 'Raleway',
                   fontWeight: FontWeight.bold,
                   letterSpacing: 1.5,
+                ),
+              ),
+              SizedBox(
+                child: Icon(
+                  Icons.more_horiz,
+                  size: 60,
+                  color: Colors.grey[300],
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                //padding: EdgeInsets.all(15),
+                child: Padding(
+                  padding: EdgeInsets.all(5),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      size: 30,
+                      color: Colors.grey[600],
+                    ),
+                    title: Text(
+                      '(516) 637-7011',
+                      style: TextStyle(
+                        color: Colors.grey[600],
+                        fontSize: 20,
+                        fontFamily: 'Raleway',
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                //padding: EdgeInsets.all(15),
+                child: Padding(
+                  padding: EdgeInsets.all(5),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      size: 30,
+                      color: Colors.grey[600],
+                    ),
+                    title: Text(
+                      'nathanrth15@gmail.com',
+                      style: TextStyle(
+                        color: Colors.grey[600],
+                        fontSize: 20,
+                        fontFamily: 'Raleway',
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
                 ),
               ),
             ],
